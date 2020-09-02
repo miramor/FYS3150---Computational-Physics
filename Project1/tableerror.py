5dded2a92918187ef40b71e377fbbe77bf54be79
miram <miram@student.matnat.uio.no>
2020-09-02 15:46:48 +0200

    Working errortable

--- Project1/tableerror.py
+++ Project1/tableerror.py
@@ -4,21 +4,22 @@ import matplotlib.pyplot as plt
 
 n_vals = [10, 10e1, 10e2] #add more values when calculated
 dict = {}
-dfN = pd.DataFrame(n_vals)
-dfError = pd.DataFrame([])
+errorlist = []
+nlist = []
 
+#fetch first errorvalue from each file, each column
 for n in n_vals:
     # Read in the first line of each results for the different n values and just use the log error column
     df = pd.read_csv(f"Results nval = {int(n)}", names = ['x','solution', 'exact', 'log error'], usecols = ["log error"], nrows = 1)
-    error = [df["log error"].values[0]]
-    dfError.append(error)
+    error = df["log error"].iloc[0]
+    errorlist.append(error)
+dfError = pd.DataFrame({"n":n_vals,"log error":errorlist})
 
-print(dfError)
 
 
 #Make table for logerror
-fig,  ax = plt.subplots(1,1)
-data1 = df[["x", "log error"]].head(6)
+fig,  ax = plt.subplots(1,1,figsize=(5,2))
+data1 = dfError[["n", "log error"]]
 #vertical_stack.style.format("{:.2%}")
 ax.axis("off")
 pd.plotting.table(ax, data1, loc="upper right", colWidths=[0.3, 0.3, 0.3])
