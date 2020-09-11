11c183f0f95d47b80240a7ce43acc702191659c7
Elias Tidemand ruud <Elias@eduroam-193-157-240-208.wlan.uio.no>
2020-09-11 17:12:55 +0200

    main

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,9 +1,6 @@
 int main(int argc, char const *argv[]) {
 
-  //Run a loop to rotate until we reach max iterations or reach limit
-  while ( fabs(max_offdiag) > epsilon && (double) iterations < max_number_iterations ) {
-    max:offdiag = maxoffdiag ( A, &k, &l, n );
-    rotate ( A, R, k, l, n );
-    iterations++;
-  return 0;
+  JacobiEigenSolve jes;
+
+  jes.Initialize(1.2, 2.3, 6, 6);
 }
