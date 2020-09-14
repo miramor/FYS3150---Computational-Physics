6d48fa3f123d2bdc4df6f881dc473cf8adc70f18
Elias Tidemand ruud <Elias@eduroam-193-157-193-191.wlan.uio.no>
2020-09-14 15:59:31 +0200

    Fixed errors and make A array

--- Project2/jacobiAlgo.cpp
+++ Project2/jacobiAlgo.cpp
@@ -12,35 +12,36 @@ using namespace arma;
 
 void JacobiEigenSolve::Initialize(double a_val, double b_val, int max_ite, int n_val){
   //Set class variables
-  a = a_val; b = b_val; max_iterations = max_ite; n = n_val
+  a = a_val; b = b_val; max_iterations = max_ite; n = n_val;
+  max_iterations = (double) n * (double) n * (double) n;
+  A = zeros<mat>(n,n);
+  V = zeros<mat>(n,n);
 
   //Use this to in a test to compare
   /*vec X(n); X.fill(a_val);
   vec Y(n); Y.fill(b_val);
-  A = toeplitz(X,Y);
-  A = mat(n,n);
+  A = toeplitz(X,Y);
+  A = mat(n,n);
   */
-  //Setup the A matrix.
+  //Setup the A matrix.
   // Not finished, find a better way to fill it
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-      if( i == j){
-        A[i][j] = b_val ;
-      }
-      if( i == j+1);
-      }
+  for(int d=0 ; d < n ; d++){
+    A(d,d)=b_val;
+    for(int j=d+1; j < n; j++){
+      A(d,j) = a_val;
+        A(j,d)=A(d,j);
     }
   }
-
+  cout << A << endl;
 }
 
-// Returns a tuple of value of element and its index. (val, row, column) => f.eks (2.2, 1, 3)
-tuple<double, int, int> FindMaxEle(Mat<double> A){
+// Returns a tuple of value of element and its index. (val, row, column) => f.eks (2.2, 1, 3)
+tuple<double, int, int> JacobiEigenSolve::FindMaxEle(Mat<double> A){
   double max = 0.0;
   for (int i =0; i <n; i++){
     for (int j = i+1; j<n; j++){
-      if ( fabs(A[i][j]) > max){
-        max = fabs(A[i][j]);
+      if ( fabs(A(i,j)) > max){
+        max = fabs(A(i,j));
       }
     }
   }
@@ -49,16 +50,16 @@ tuple<double, int, int> FindMaxEle(Mat<double> A){
 }
 
 // finds the value of cos(theta) and sin(theta) and fills up V each time
-void Rotate(Mat<double> A, int row, int col){
+void JacobiEigenSolve::Rotate(Mat<double> A, int row, int col){
 
 }
 
  //Runs the rotation until we reached the max ite or reached the eps
-void Solve(){
+void JacobiEigenSolve::Solve(){
 
 }
 
-// Prints A, used for checks
-Mat<double> PrintA(){
+// Prints A, used for checks
+Mat<double> JacobiEigenSolve::PrintA(){
   cout << "Matrix A: " << A << endl;
 }
