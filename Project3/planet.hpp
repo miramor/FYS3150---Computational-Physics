c05b6f55d85326a4924d86954777ae5a91d1f229
Elias Tidemand ruud <Elias@eduroam-193-157-178-146.wlan.uio.no>
2020-10-02 13:28:38 +0200

    Project 3 folder

---
+++ Project3/planet.hpp
@@ -0,0 +1,18 @@
+#ifndef PLANET.HPP
+#define PLANET_HPP
+
+#include <vector>
+#include <string.h>
+#include <tuple>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+class Planet{
+  private:
+    
+
+  public:
+
+}
