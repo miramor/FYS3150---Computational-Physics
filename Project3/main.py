33c1fe917417cec803d4d66e5d7bd1aa35017d59
Bendik <bendik98ny@gmail.com>
2020-10-02 14:17:31 +0200

    update

---
+++ Project3/main.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import matplotlib.pyplot as plt
+
+try:
+    if sys.argv[1] == "test":
+        os.system("echo compiling...")
+        os.system("c++ test.exe test.cpp") #compile codes
+
+        os.system("echo executing...")
+        os.system("./test.exe ")
+
+except:
+    os.system("echo compiling...")
+    os.system("make") #compile codes
+
+    os.system("echo executing...")
+    os.system("./output.exe")
