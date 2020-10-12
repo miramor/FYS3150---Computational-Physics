c2610c0b8ad23de2b59a8a01c6cee2103a1f7d3b
Bendik <bendik98ny@gmail.com>
2020-10-12 13:14:03 +0200

    Update, working code

--- Project3/plot_results.py
+++ Project3/plot_results.py
@@ -2,13 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+systems = {
+"systemA": ["Sun", "Earth"],
+"systemB": ["Sun", "Earth", "Jupiter"],
+"systemC": ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+"systemD": ["Sun", "Mercury", "Earth"]
+}
+parameters = ['x_','y_','z_','vx_','vy_','vz_']
 
-earth_data = pd.read_csv("Results/systemA.csv", index_col=False, names=['sx', 'sy', 'sz', 'svx', 'svy', 'svz','x', 'y', 'z', 'vx', 'vy', 'vz'])
+def plot_sys(system):
+    sys_names = [par + obj for obj in systems[system] for par in parameters]
+    print(sys_names)
+    sys_data = pd.read_csv("Results/" + system + ".csv", index_col=False, names=sys_names)
+    for i in range(int(len(sys_names)/6)):
+        x = sys_data[sys_names[i*6]]
+        y = sys_data[sys_names[i*6+1]]
+        plt.plot(x,y, label = systems[system][i])
 
-x = earth_data['x'].to_numpy()
-y = earth_data['y'].to_numpy()
+    plt.axis('equal')
+    plt.legend()
+    plt.title('%s' %(system))
+    plt.savefig("Plots/" + system + ".png", dpi=400)
 
-print(earth_data.head())
-plt.plot(x, y)
-plt.axis('equal')
-plt.savefig("sun_earth.png")
+plot_sys("systemD")
